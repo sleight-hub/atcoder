bb428cab9f9d03c22c9ed6900c20dbeb9838a7d2
sleight <cats.cafemocha+sleight@gmail.com>
2020-02-28 12:37:08 +0900

    20200228_03

---
+++ abc154/abc154_b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  // 文字列の入力
+  string str;
+  cin >> str;
+
+  for (int i = 0; i < str.length(); i++) {
+    cout << 'x';
+  }
+  // 出力
+  cout << endl;
+  return 0;
+}
