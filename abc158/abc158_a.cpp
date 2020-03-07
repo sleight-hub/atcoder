c5413ad7598a6014bbbbd79cd33e157c6ba312b8
sleight <cats.cafemocha+sleight@gmail.com>
2020-03-07 22:55:43 +0900

    20200307_01

---
+++ abc158/abc158_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  // 文字列の入力
+  string str;
+  cin >> str;
+
+  if (str == "AAA" || str == "BBB") {
+    cout << "No" << endl;
+    return 0;
+  }
+  // 出力
+  cout << "Yes" << endl;
+  return 0;
+}
