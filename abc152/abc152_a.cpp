2db66f0f0a709adf49e0d55c3fd5f0385801b2fc
sleight <cats.cafemocha+sleight@gmail.com>
2020-03-13 01:26:52 +0900

    20200301_01

---
+++ abc152/abc152_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  // 文字列の入力
+  int n, m;
+  cin >> n >> m;
+
+  if (n == m) {
+    // 出力
+    cout << "Yes" << endl;
+  } else {
+    // 出力
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
