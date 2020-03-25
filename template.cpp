43e4378407ec8de927816dc2ae66d868a0a71df7
sleight <cats.cafemocha+sleight@gmail.com>
2020-03-26 00:34:30 +0900

    20200326_template

--- template.cpp
+++ template.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
 int main() {
-  // 文字列の入力
   string str;
   cin >> str;
 
   int ans = 0;
-  // 出力
+
   cout << ans << endl;
   return 0;
 }
