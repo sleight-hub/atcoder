d8445fff60e28768c06f284226433f0f15c829ac
sleight <cats.cafemocha+sleight@gmail.com>
2020-03-28 22:48:29 +0900

    20200328_01

---
+++ abc160/abc160_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  string s;
+  cin >> s;
+
+  if (s[2] == s[3] && s[4] == s[5]) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
