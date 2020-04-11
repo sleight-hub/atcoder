e05e64fb1e82a1ed805b0953c6cf9be90b66933b
sleight <cats.cafemocha+sleight@gmail.com>
2020-04-11 12:16:45 +0900

    20200411_01

---
+++ abc139/abc139_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  int ans = 0;
+  for (int i = 0; i < 3; i++) {
+    if (s[i] == t[i]) {
+      ans++;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
