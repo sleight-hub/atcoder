e0292eac0f24567cf74e7e756a646ef4a745d4af
sleight <cats.cafemocha+sleight@gmail.com>
2020-04-12 15:18:16 +0900

    20200412_01

---
+++ abc138/abc138_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a;
+  string s;
+  cin >> a >> s;
+
+  if (a >= 3200) {
+    cout << s << endl;
+    return 0;
+  }
+
+  cout << "red" << endl;
+  return 0;
+}
