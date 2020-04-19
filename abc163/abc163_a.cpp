9723f4ddacb5b8488749686089b098fd141bbcf1
sleight <cats.cafemocha+sleight@gmail.com>
2020-04-19 22:56:22 +0900

    20200419_02

---
+++ abc163/abc163_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double PI = 3.14159265358979323846;
+typedef long long ll;
+
+int main() {
+  double r;
+  cin >> r;
+
+  cout << setprecision(20) << 2 * r * PI << endl;
+  return 0;
+}
