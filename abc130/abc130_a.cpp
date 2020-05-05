18d6a534b761d25b697672a1680cb56b882e4f96
sleight <cats.cafemocha+sleight@gmail.com>
2020-05-05 22:40:10 +0900

    20200505_01

---
+++ abc130/abc130_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int x, a;
+  cin >> x >> a;
+
+  if (x < a) {
+    cout << 0 << endl;
+  } else {
+    cout << 10 << endl;
+  }
+  return 0;
+}
