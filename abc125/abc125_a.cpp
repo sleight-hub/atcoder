5f8dfbdaef426ae863e716c5ac18d0cfb78c39e1
sleight <cats.cafemocha+sleight@gmail.com>
2020-05-15 23:51:35 +0900

    20200515_01

---
+++ abc125/abc125_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a, b, t;
+  cin >> a >> b >> t;
+
+  int ans = 0;
+
+  cout << (t / a) * b << endl;
+  return 0;
+}
