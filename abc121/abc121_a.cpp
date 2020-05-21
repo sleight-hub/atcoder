f0dbac2b1442ef8f493143a81f33ea56e4b3e131
sleight <cats.cafemocha+sleight@gmail.com>
2020-05-21 23:56:12 +0900

    20200521_01

---
+++ abc121/abc121_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int H, W, h, w;
+
+  cin >> H >> W >> h >> w;
+
+  cout << (H - h) * (W - w) << endl;
+  return 0;
+}
