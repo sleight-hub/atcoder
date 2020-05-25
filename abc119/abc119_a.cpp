d152304f025756f35b7f832fb58cf785f8778ea7
sleight <cats.cafemocha+sleight@gmail.com>
2020-05-25 23:49:18 +0900

    20200525_01

---
+++ abc119/abc119_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  string s;
+  cin >> s;
+
+  if (s <= "2019/04/30") {
+    cout << "Heisei" << endl;
+  } else {
+    cout << "TBD" << endl;
+  }
+  return 0;
+}
