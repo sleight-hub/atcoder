c6a56d9d633d3b488548fa81d343ecf076367e28
sleight <cats.cafemocha+sleight@gmail.com>
2020-05-27 23:51:29 +0900

    20200527_01

---
+++ abc117/abc117_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  double t, x;
+  cin >> t >> x;
+
+  cout << fixed << setprecision(16) << t / x << endl;
+  return 0;
+}
