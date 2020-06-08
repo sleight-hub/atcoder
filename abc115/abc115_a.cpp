c5e4e5030b6105d7e93abd9ef6fd522ac45c06af
sleight <cats.cafemocha+sleight@gmail.com>
2020-06-08 12:35:19 +0900

    20200608_01

---
+++ abc115/abc115_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int d;
+  cin >> d;
+
+  if (d == 22)
+    cout << "Christmas Eve Eve Eve" << endl;
+  else if (d == 23)
+    cout << "Christmas Eve Eve" << endl;
+  else if (d == 24)
+    cout << "Christmas Eve" << endl;
+  else if (d == 25)
+    cout << "Christmas" << endl;
+
+  return 0;
+}
