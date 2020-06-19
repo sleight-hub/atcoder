184ac7c31fc88f33b6cba394e1c0520d6bd8f38c
sleight <cats.cafemocha+sleight@gmail.com>
2020-06-19 23:01:55 +0900

    20200619_01

---
+++ abc109/abc109_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a % 2 == 1 && b % 2 == 1) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
