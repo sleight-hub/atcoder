9cfd514a76175083d6b9e07df3a9d68cd9c84c0d
sleight <cats.cafemocha+sleight@gmail.com>
2020-06-21 22:53:31 +0900

    20200621_02

---
+++ abc171/abc171_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  char a;
+  cin >> a;
+
+  if ('A' <= a && a <= 'Z') {
+    cout << 'A' << endl;
+  } else {
+    cout << 'a' << endl;
+  }
+  return 0;
+}
