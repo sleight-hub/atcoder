a8d000dff30c6ce957aee51498f9e9af5565d5bd
sleight <cats.cafemocha+sleight@gmail.com>
2020-06-28 12:05:04 +0900

    20200628_02

---
+++ abc172/abc172_b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  int ans = 0;
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] != t[i]) {
+      ans++;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
