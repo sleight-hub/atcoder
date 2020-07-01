d61d4a1fedc77a7af21ebf1cccc4ba4c03078340
sleight <cats.cafemocha+sleight@gmail.com>
2020-07-01 22:40:47 +0900

    20200701_01

---
+++ abc104/abc104_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int r;
+  cin >> r;
+  if (r < 1200) {
+    cout << "ABC" << endl;
+  } else if (r < 2800) {
+    cout << "ARC" << endl;
+  } else {
+    cout << "AGC" << endl;
+  }
+  return 0;
+}
