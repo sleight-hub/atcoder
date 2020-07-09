6451e39caf0ca9b870ce6c68a99fa76a2c03ffc2
sleight <cats.cafemocha+sleight@gmail.com>
2020-07-09 12:21:10 +0900

    20200709_01

---
+++ abc100/abc100_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a <= 8 && b <= 8) {
+    cout << "Yay!" << endl;
+  } else {
+    cout << ":(" << endl;
+  }
+  return 0;
+}
