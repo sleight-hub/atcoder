b1dfd29559a7333c94c9d915a475fca3d3f1b257
sleight <cats.cafemocha+sleight@gmail.com>
2020-07-10 12:05:16 +0900

    20200710_01

---
+++ abc099/abc099_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n < 1000) {
+    cout << "ABC";
+  } else {
+    cout << "ABD";
+  }
+  return 0;
+}
