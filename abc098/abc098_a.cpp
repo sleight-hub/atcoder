92d27ddf8a8406013478203a39b3f0ac5ce5bb20
sleight <cats.cafemocha+sleight@gmail.com>
2020-07-11 19:09:53 +0900

    20200711_01

---
+++ abc098/abc098_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  cout << max({a + b, a - b, a * b}) << endl;
+  return 0;
+}
