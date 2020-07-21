df141d539c6a02596ae471e98217c50cc188de79
sleight <cats.cafemocha+sleight@gmail.com>
2020-07-21 09:38:59 +0900

    20200721_01

---
+++ abc090/abc090_a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  string s[3];
+  cin >> s[0] >> s[1] >> s[2];
+  cout << s[0][0] << s[1][1] << s[2][2] << endl;
+  return 0;
+}
