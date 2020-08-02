2a087a5eb0ed6b90ce9e9fedc69025fc4ce93ced
sleight <cats.cafemocha+sleight@gmail.com>
2020-08-03 00:28:29 +0900

    20200803_01

---
+++ abc174/abc174_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int x;
+  cin >> x;
+
+  if (x >= 30) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
