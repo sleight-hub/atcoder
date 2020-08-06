4d1502b65a1ec360d80936aafd6817df2628e785
sleight <cats.cafemocha+sleight@gmail.com>
2020-08-06 22:20:31 +0900

    20200806_01

---
+++ abc073/abc073_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  char n1, n2;
+  cin >> n1 >> n2;
+
+  if (n1 == '9' || n2 == '9') {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
