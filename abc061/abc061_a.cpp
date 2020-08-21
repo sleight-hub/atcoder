0634816efcd243b39665c526b029723d68094d44
sleight <cats.cafemocha+sleight@gmail.com>
2020-08-22 00:48:16 +0900

    20200822_01

---
+++ abc061/abc061_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  if (a <= c && c <= b) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
