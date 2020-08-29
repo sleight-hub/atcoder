044996554e65e98a0e2fe962b2edfe63a67934ad
sleight <cats.cafemocha+sleight@gmail.com>
2020-08-30 05:18:05 +0900

    20200830_01

---
+++ abc177/abc177_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int d, t, s;
+  cin >> d >> t >> s;
+  if (d <= t * s) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
