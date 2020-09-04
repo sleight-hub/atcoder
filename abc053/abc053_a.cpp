0cfb4176a39cf0ede5ac6b4039e5b1488f7d4588
sleight <cats.cafemocha+sleight@gmail.com>
2020-09-05 01:07:36 +0900

    20200905_01

---
+++ abc053/abc053_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int x;
+  cin >> x;
+
+  if (x < 1200)
+    cout << "ABC" << endl;
+  else
+    cout << "ARC" << endl;
+
+  return 0;
+}
