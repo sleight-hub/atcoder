a116a90548e2eb0620fd9abc8650d754cf0e1a45
sleight <cats.cafemocha+sleight@gmail.com>
2020-09-06 02:04:19 +0900

    20200906_01

---
+++ abc052/abc052_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  cout << max(a * b, c * d) << endl;
+  return 0;
+}
